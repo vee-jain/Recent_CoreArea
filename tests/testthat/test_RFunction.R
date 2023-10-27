66a0b0951fc0816a1f6e607bab307330a1677786
vee-jain <92858041+vee-jain@users.noreply.github.com>
2023-10-27 13:45:09 +0200

    Update test_RFunction.R

--- tests/testthat/test_RFunction.R
+++ tests/testthat/test_RFunction.R
@@ -1,13 +1,8 @@
 library('move2')
 
 test_data <- test_data("input3_move2.rds") #file must be move2!
+actual <- rFunction(data = test_data, days_prior = 14)
 
 test_that("happy path", {
-  actual <- rFunction(data = test_data, sdk = "unit test", year = 2005)
-  expect_equal(unique(lubridate::year(mt_time(actual))), 2005)
-})
-
-test_that("year not included", {
-  actual <- rFunction(data = test_data, sdk = "unit test", year = 2023)
-  expect_null(actual)
+  expect_equal(test_data,actual)
 })
